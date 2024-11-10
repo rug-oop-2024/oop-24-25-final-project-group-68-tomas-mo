8b5d571333ce3af9f85bcf928011d0ee5c0e66ee
tomasderijk <tomasderijk@gmail.com>
2024-11-10 22:40:09 +0100

    flake 8

--- autoop/core/ml/model/__init__.py
+++ autoop/core/ml/model/__init__.py
@@ -1,13 +1,46 @@
-
 from autoop.core.ml.model.model import Model
-from autoop.core.ml.model.regression import MultipleLinearRegression
+from autoop.core.ml.model.classification import (
+    LogisticRegressionModel,
+    DecisionTreeClassification,
+    RandomForestClassification
+)
+
+from autoop.core.ml.model.regression import (
+    MultipleLinearRegression,
+    DecisionTreeRegression,
+    RandomForestRegression,
+    SVRRegression
+)
 
 REGRESSION_MODELS = [
-] # add your models as str here
+    "Multiple Linear Regression",
+    "Decision Tree Regression",
+    "Random Forest Regression",
+    "SVR Regression"
+]
 
 CLASSIFICATION_MODELS = [
-] # add your models as str here
+    "Logistic Regression",
+    "Decision Tree Classification",
+    "Random Forest Classification"
+]
+
 
 def get_model(model_name: str) -> Model:
     """Factory function to get a model by name."""
-    raise NotImplementedError("To be implemented.")
+    if model_name == "Logistic Regression":
+        return LogisticRegressionModel()
+    elif model_name == "Decision Tree Classifier":
+        return DecisionTreeClassification()
+    elif model_name == "Random Forest Classifier":
+        return RandomForestClassification()
+    elif model_name == "Multiple Linear Regression":
+        return MultipleLinearRegression()
+    elif model_name == "Decision Tree Regression":
+        return DecisionTreeRegression()
+    elif model_name == "Random Forest Regression":
+        return RandomForestRegression()
+    elif model_name == "SVR Regression":
+        return SVRRegression()
+    else:
+        raise ValueError(f"Model '{model_name}' isn't recognised")
